7aa25a7c3f51dfc192c864d8c29c2f5dc12959e2
Higr25 <shadowraiderr@seznam.cz>
2019-06-03 08:01:00 +0200

    admins can now create events

--- app/presenters/TheatrePresenter.php
+++ app/presenters/TheatrePresenter.php
@@ -85,8 +85,6 @@ final class TheatrePresenter extends Nette\Application\UI\Presenter
                  ->fetch();
           $isFree = $row['free'];
 
-
-
         if ($isFree) {
 
            $seatNumberArray = [
